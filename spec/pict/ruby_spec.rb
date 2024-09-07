ac42678054d24a809ff8990ef37b5a591ce80c13
shichida.masatsugu <shichida.masatsugu@moneyforward.co.jp>
2024-09-07 23:02:25 +0900

    first commit

---
+++ spec/pict/ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pict::Ruby do
+  it "has a version number" do
+    expect(Pict::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
