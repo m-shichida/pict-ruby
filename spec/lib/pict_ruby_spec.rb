475374de26b2803c4fd23909e9d8647163996781
Shichida.Masatsugu <shicchi.7913@icloud.com>
2024-09-08 15:54:00 +0900

    remove test

--- spec/lib/pict_ruby_spec.rb
+++ spec/lib/pict_ruby_spec.rb
@@ -6,24 +6,4 @@ RSpec.describe PictRuby do
       expect(PictRuby::VERSION).to be('0.1.0')
     end
   end
-
-  describe '.generate_test_cases' do
-    let(:params) do
-      [
-        %w[iPhone Pixel],
-        %w[IOS Android],
-        %w[Firefox Chrome]
-      ]
-    end
-
-    it 'returns test cases' do
-      actual = PictRuby.generate_test_cases(params)
-      expect(actual).to eq(
-        [{ 'param1' => 'Pixel', 'param2' => 'Android', 'param3' => 'Chrome' },
-         { 'param1' => 'Pixel', 'param2' => 'IOS', 'param3' => 'Firefox' },
-         { 'param1' => 'iPhone', 'param2' => 'IOS', 'param3' => 'Chrome' },
-         { 'param1' => 'iPhone', 'param2' => 'Android', 'param3' => 'Firefox' }]
-      )
-    end
-  end
 end
