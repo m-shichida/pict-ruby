6610673ea1efa65fc59b2c8fbcb2a141c8649398
Shichida.Masatsugu <shicchi.7913@icloud.com>
2024-09-08 10:14:59 +0900

    shape output

--- lib/pict_ruby.rb
+++ lib/pict_ruby.rb
@@ -17,20 +17,9 @@ module PictRuby
         output, status = Open3.capture2("#{PICT_EXEC_PATH} #{tempfile.path}")
         raise "PICT execution failed. output: #{output}" unless status.success?
 
-        output
+        Output.parse(output)
       ensure
         tempfile.unlink
-      end
-    end
-
-    private
-
-    def parse_output(output)
-      lines = output.split("\n")
-      headers = lines.shift.split("\t")
-      lines.map do |line|
-        values = line.split("\t")
-        headers.zip(values).to_h
       end
     end
   end
@@ -40,6 +29,17 @@ module PictRuby
       parameter.map.with_index do |param, index|
         "param#{index + 1}: #{param.join(',')}"
       end.join("\n")
+    end
+  end
+
+  module Output
+    def self.parse(output)
+      lines = output.split("\n")
+      headers = lines.shift.split("\t")
+      lines.map do |line|
+        values = line.split("\t")
+        headers.zip(values).to_h
+      end
     end
   end
 end
