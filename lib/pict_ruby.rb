3bada15b753f28f96a607f48e1d4e1314e940e2f
Shichida.Masatsugu <shicchi.7913@icloud.com>
2024-09-08 10:11:11 +0900

    separate shaping parameter

--- lib/pict_ruby.rb
+++ lib/pict_ruby.rb
@@ -6,28 +6,20 @@ module PictRuby
   PICT_EXEC_PATH = File.expand_path(File.join(__dir__, '..', 'tools', 'pict'))
 
   class << self
-    def generate_test_cases
-      parameters = [
-        ["iPhone", "Pixel", "XPERIA"],
-        ["IOS", "Android"],
-        ["Firefox", "Chrome"]
-      ]
-
-      input = parameters.map.with_index do |param, index|
-        "param#{index + 1}: #{param.join(',')}"
-      end.join("\n")
+    def generate_test_cases(params)
+      input = Parameter.shape(params)
 
       tempfile = Tempfile.new('pict_input')
       begin
         tempfile.write(input)
         tempfile.close
-        
+
         output, status = Open3.capture2("#{PICT_EXEC_PATH} #{tempfile.path}")
         raise "PICT execution failed. output: #{output}" unless status.success?
 
         output
       ensure
-        tempfile.unlink 
+        tempfile.unlink
       end
     end
 
@@ -40,6 +32,14 @@ module PictRuby
         values = line.split("\t")
         headers.zip(values).to_h
       end
+    end
+  end
+
+  module Parameter
+    def self.shape(parameter)
+      parameter.map.with_index do |param, index|
+        "param#{index + 1}: #{param.join(',')}"
+      end.join("\n")
     end
   end
 end
